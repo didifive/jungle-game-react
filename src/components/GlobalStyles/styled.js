cd9395fba6d9bb9808c1826a4cfc8ab836fb5031
Luis Carlos <luisczdidi@gmail.com>
2021-10-16 12:14:30 -0300

    Corrected background

--- src/components/GlobalStyles/styled.js
+++ src/components/GlobalStyles/styled.js
@@ -3,11 +3,11 @@ import bgImg from '../../assets/img/background/bg.png';
 
 export const GlobalStyle = createGlobalStyle`
   body {
-    background-color: #cccccc;
+    background-color: #eeeeee;
     background: url(${bgImg}) center repeat;  
-    color: #333333;
+    color: #000000;
     padding: 0;
     margin: 0;
-    font-family: 'Luckiest Guy', cursive;
+    font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
   }
 `;
--- src/components/Scenario/styled.js
+++ src/components/Scenario/styled.js
@@ -4,7 +4,6 @@ export const ScenarioStyled = styled.div`
   position: absolute;
   bottom: 0;
   left: 0;
-  background-color: #ffffff;
   width: 100vw;
   height: 100vh;
   z-index: -99;
